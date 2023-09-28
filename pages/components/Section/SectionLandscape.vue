360c8bba9bc34d5b177853671f5e49acdb14e0d4
jlpereira <pepelui@outlook.com>
2023-09-28 19:02:56 -0300

    Fix responsive

--- pages/components/Section/SectionLandscape.vue
+++ pages/components/Section/SectionLandscape.vue
@@ -3,12 +3,12 @@
     <div class="relative box-border">
       <ImageCarrousel>
         <div
-          class="flex flex-col justify-center items-center w-full h-full gap-4 bg-black bg-opacity-25"
+          class="flex flex-col justify-center sm:items-center w-full h-full gap-4 bg-black bg-opacity-25"
         >
-          <span class="text-4xl font-medium text-white">{{
+          <span class="text-3xl sm:text-4xl px-4 font-medium text-white">{{
             project_name
           }}</span>
-          <span class="text-xl text-white"
+          <span class="text-lg sm:text-xl text-white px-4"
             >Taxonomic database of the world's grasshoppers, locusts, katydids,
             crickets, and related insects</span
           >
